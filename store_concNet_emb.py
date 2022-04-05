5a234c4b2248b3a310dca9b272bc2fa61a50d90f
watermeleon <leoneshuijs@gmail.com>
2022-04-05 15:31:31 +0200

    compare clipfeat notebook, stor CN word embs

--- store_concNet_emb.py
+++ store_concNet_emb.py
@@ -1,5 +1,5 @@
 
-from knowgraph_conceptnet import KnowledgeGraph
+# from knowgraph_conceptnet import KnowledgeGraph
 # from data.utils import *
 import numpy as np
 # from scipy import spatial
@@ -10,7 +10,7 @@ import torch
 # from transformers import AutoTokenizer
 import csv
 from collections import defaultdict
-
+import clip
 
 print("hey")
 all_conceptnetwords = set()
@@ -32,14 +32,21 @@ print(len(all_conceptnetwords))
 # device = torch.device('cpu')
 device = torch.device('cuda')
 
+clipmodel = "clip_github"
 
-# model_txt_hf = CLIPTextModel.from_pretrained("openai/clip-vit-base-patch32").to(device)
-model_clip = CLIPModel.from_pretrained("openai/clip-vit-base-patch32").to(device)
-tokenizer = CLIPTokenizer.from_pretrained("openai/clip-vit-base-patch32")
+if clipmodel == "huggingface":
+    model_clip = CLIPModel.from_pretrained("openai/clip-vit-base-patch32").to(device)
+    tokenizer = CLIPTokenizer.from_pretrained("openai/clip-vit-base-patch32")
+elif clipmodel == "clip_github":
+    model, preprocess = clip.load("RN50x4", device=device)
+else:
+    print("unkown clipmodel: use huggingface or github model")
+    quit()
+
 
 pick_dict = {}
 i = 0
-out_path = "conceptNet_embedding_hf_lisa_2.pkl"
+out_path = "conceptNet_embedding_rn50x4.pkl"
 file_to_store = open(out_path, "wb")
 
 word_prefix = ""
@@ -47,14 +54,16 @@ word_prefix = ""
 for word_list in all_conceptnetwords:
     word = word_prefix + word_list
     # word = "golden gate bridge"
-    word_tok =   tokenizer(word, padding=True, return_tensors="pt").to(device)
-    # with torch.no_grad():
-    #     prefix = model_clip(**word_tok)
-    # prefix = prefix.pooler_output.cpu().numpy()[0]
-    prefix = model_clip.get_text_features(**word_tok)
+    with torch.no_grad():
+        if clipmodel == "huggingface":
+            word_tok =   tokenizer(word, padding=True, return_tensors="pt").to(device)
+            prefix = model_clip.get_text_features(**word_tok)
+            pefix =  prefix.squeeze().detach().cpu().numpy()
+        else:
+            word_tok = clip.tokenize(word).to(device)
+            prefix  = model.encode_text(word_tok)
 
-
-    pick_dict[word] = prefix.squeeze().detach().cpu().numpy()
+    pick_dict[word] = prefix
 
     if i%1000 == 0:
         print(i)
