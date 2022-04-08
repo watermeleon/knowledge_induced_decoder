f4d8ccbface1ebc07f543d215a1036504ae47fab
watermeleon <leoneshuijs@gmail.com>
2022-04-08 15:19:09 +0200

    CN fused, using functions and for rn50x4

--- CN_fuse_nesteddict.py
+++ CN_fuse_nesteddict.py
@@ -5,65 +5,72 @@ import numpy as np
 from transformers import  CLIPTokenizer
 import pickle
 import torch
-
-tokenizerBW =  CLIPTokenizer.from_pretrained("openai/clip-vit-base-patch32")
-
-pth_clipemb = "../data_files/concNet_filtBanana_save.pkl"
-# pth_clipemb = "/media/leonardo/Floppy/concNet_filtBanana_save.pkl"
-with open(pth_clipemb, 'rb') as f:
-            CN_dict = pickle.load(f)
+import argparse
 
 
-# load the stored embeddings:
-# cn_wordfeats_path = "/media/leonardo/Floppy/conceptNet_embedding_hf_lisa_2.pkl"
-cn_wordfeats_path = "../Datasets/conceptNet_embedding_hf_lisa_2.pkl"
 
-with open(cn_wordfeats_path, 'rb') as f:
-            cn_wordfeats = pickle.load(f)
-cn_wordfeats =  {str(k).encode('latin-1').decode('utf-8') : v for k,v in cn_wordfeats.items()}
+def create_nested(clipmodel):
+    # load the nested dict
+    pth_clipemb = "../data_files/concNet_filtBanana_save.pkl"
+    with open(pth_clipemb, 'rb') as f:
+                CN_dict = pickle.load(f)
+
+    if clipmodel == "huggingface":
+        tokenizerBW =  CLIPTokenizer.from_pretrained("openai/clip-vit-base-patch32")
+        cn_wordfeats_path = "../Datasets/conceptNet_embedding_hf_lisa_2.pkl"
+        out_path = "../data_files/concNetFilt_emb_Banana_lisa2.pkl"
+
+    else: 
+        cn_wordfeats_path = "../data_files/conceptNet_embedding_rn50x4.pkl"
+        out_path = "../data_files/concNet_nested_emb_rn50x4.pkl"
+
+    # load the stored embeddings:
+    with open(cn_wordfeats_path, 'rb') as f:
+                cn_wordfeats = pickle.load(f)
+    if clipmodel == "huggingface":
+        cn_wordfeats =  {str(k).encode('latin-1').decode('utf-8') : v for k,v in cn_wordfeats.items()}
+
+    file_to_store = open(out_path, "wb")
+
+    # device = torch.device('cpu')
+    # device = torch.device('cuda')
+
+    CN_wordsNemb_dict = {}
+    totemb = 0
+    for i, (keyword, relword_list) in enumerate(CN_dict.items()):
+
+        rw_emb_list = []
+        for relword_item in relword_list:
+            # l2r edge is 0, so need to invert the binary
+            rw_idx = int(not(relword_item[-1]))
+            related_word = relword_item[rw_idx]
+
+            # assert related_word in cn_wordfeats, "related word not in wordfeats"
+            try:
+                rw_emb = cn_wordfeats[related_word]
+            except:
+                print("kw, relword:", keyword, relword_item)
+                quit()
+
+            rw_emb_list.append([relword_item, rw_emb])
+            totemb += 1
+
+        CN_wordsNemb_dict[keyword] = np.array(rw_emb_list)
+        if i%100 == 0:
+            print(i, totemb)
+        # break
+    print(totemb)
+    pickle.dump(CN_wordsNemb_dict, file_to_store)
 
 
-# device = torch.device('cpu')
-device = torch.device('cuda')
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--clipmodel', default="clip_github", choices=('clip_github','huggingface'))
+    parser.add_argument('--createfile', default="nested_with_emb", choices=('nested_with_emb','nested_pretok'))
 
-
-CN_wordsNemb_dict = {}
-i = 0
-out_path = "concNetFilt_emb_Banana_lisa2.pkl"
-
-file_to_store = open(out_path, "wb")
-
-# word_prefix = ""
-totemb = 0
-for keyword, relword_list in CN_dict.items():
-
-    rw_emb_list = []
-    for relword_item in relword_list:
-        # l2r edge is 0, so need to invert the binary
-        rw_idx = int(not(relword_item[-1]))
-        related_word = relword_item[rw_idx]
-                
-        if related_word not in cn_wordfeats:
-            print("not in wordfeats")
-        try:
-            # related_word = str(related_word).encode('latin-1').decode('utf-8')
-            rw_emb = cn_wordfeats[related_word]
-        except:
-            print("kw, relword:", keyword, relword_item)
-            quit()
-        if len(rw_emb) != 512:
-            print("len is not 512, emb is:", rw_emb)
-
-
-        rw_rel = relword_item
-
-        rw_emb_list.append([rw_rel, rw_emb])
-        totemb += 1
-
-    CN_wordsNemb_dict[keyword] = np.array(rw_emb_list)
-    if i%100 == 0:
-        print(i, totemb)
-    i+=1
-    # break
-print(totemb)
-pickle.dump(CN_wordsNemb_dict, file_to_store)
+    args = parser.parse_args()
+    if args.createfile == "nested_with_emb":
+        create_nested(args.clipmodel)
+    elif args.createfile == "nested_pretok":
+        create_pretok(args.clipmodel)
+    # exit(main(args.clipmodel))
