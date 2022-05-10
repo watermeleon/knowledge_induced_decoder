9cd3645dbe7181fd41927c529945fd880910598a
watermeleon <leoneshuijs@gmail.com>
2022-05-10 14:39:34 +0200

    first nucleus then topk

--- test_gen.py
+++ test_gen.py
@@ -197,7 +197,7 @@ if os.path.exists(fname):
     dict_dataloader_test = DataLoader(dict_dataset_test, batch_size=args.batch_size, num_workers=args.workers,shuffle=True)
 
     # Sampling options: ['topk', 'beam', 'nucleus'])
-    variationlist = [("topk", 1), ("nucleus", 1), ("beam", 1)]
+    variationlist = [("nucleus", 1),("topk", 1), ("beam", 1)]
     #  ("topk", 0.2),("topk", 2)]
     for sampling_method , sampling_temp in variationlist:
         model.sampling_temp = sampling_temp
