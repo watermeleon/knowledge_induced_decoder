c831eda7e1766e5935fc48076a0383b80a4fe2ba
watermeleon <leoneshuijs@gmail.com>
2022-05-26 17:18:21 +0200

    gen nested CN file

--- feature generation/CN_fuse_nesteddict.py
+++ feature generation/CN_fuse_nesteddict.py
@@ -5,21 +5,17 @@ Input: conceptnet_filt_nest_labels.pkl
 outfile: dict[keyword] = listof : [ [tokword1, tokword2, relword_item[-1]], related_concept_embedding]
 """
 
-from knowgraph_conceptnet import KnowledgeGraph
 import numpy as np
-
-from transformers import  CLIPTokenizer, CLIPTokenizerFast
+from transformers import  CLIPTokenizerFast
 import pickle
 import torch
 import argparse
 import clip
 import ftfy
 
-allrel = ['<|Antonym|>', '<|AtLocation|>', '<|CapableOf|>', '<|Causes|>', '<|CausesDesire|>', '<|CreatedBy|>', '<|DefinedAs|>', '<|DerivedFrom|>', '<|Desires|>', '<|DistinctFrom|>', '<|Entails|>', '<|EtymologicallyDerivedFrom|>', '<|EtymologicallyRelatedTo|>', '<|FormOf|>', '<|HasA|>', '<|HasContext|>', '<|HasFirstSubevent|>', '<|HasLastSubevent|>', '<|HasPrerequisite|>', '<|HasProperty|>', '<|HasSubevent|>', '<|InstanceOf|>', '<|IsA|>', '<|LocatedNear|>', '<|MadeOf|>', '<|MannerOf|>', '<|MotivatedByGoal|>', '<|NotCapableOf|>', '<|NotDesires|>', '<|NotHasProperty|>', '<|PartOf|>', '<|ReceivesAction|>', '<|RelatedTo|>', '<|SimilarTo|>', '<|SymbolOf|>', '<|Synonym|>', '<|UsedFor|>', '<|capital|>', '<|field|>', '<|genre|>', '<|genus|>', '<|influencedBy|>', '<|knownFor|>', '<|language|>', '<|leader|>', '<|occupation|>', '<|product|>']
-allrel = [ftfy.fix_text(rel) for rel in allrel]
-print(allrel)
-
 def create_nested(clipmodel, pretok, tok_thresh):
+    clipmodel = clipmodel.replace('/', '_')
+
     print("started creating nested dict...")
     # load the nested dict
     pth_clipemb = "../data_files/CN_feats/conceptnet_filt_nest_labels.pkl"
@@ -28,23 +24,20 @@ def create_nested(clipmodel, pretok, tok_thresh):
     print("hi")
 
     pretok_label = "_pretok" if pretok else ""
-    if clipmodel == "huggingface":
+    if clipmodel == "ViT-B_32":
         cn_wordfeats_path = "../data_files/CN_feats/conceptNet_embedding_ViT.pkl"
-        # out_path = "../data_files/concNetFilt_emb_Banana_lisa2" + pretok_label + ".pkl"
         out_path = "../data_files/CN_feats/concNet_nested_emb_ViT" + pretok_label + "_maxtok.pkl"
-
     else: 
         cn_wordfeats_path = "../data_files/CN_feats/conceptNet_embedding_rn50x4.pkl"
         out_path = "../data_files/CN_feats/concNet_nested_emb_rn50x4" + pretok_label + ".pkl"
 
     # use the same tokenizer for both github and HF clip
-    tokenizerBW =  CLIPTokenizerFast.from_pretrained("openai/clip-vit-base-patch32")
-    tokenizerBW.add_tokens(allrel, special_tokens=True)
+    tokenizerBW =  CLIPTokenizerFast.from_pretrained("../models/tokenizers_stored/CLIPTokenizerFast")
     
     # load the stored embeddings:
     with open(cn_wordfeats_path, 'rb') as f:
                 cn_wordfeats = pickle.load(f)
-    if clipmodel == "huggingface":
+    if clipmodel == "ViT-B_32":
         cn_wordfeats =  {str(k).encode('latin-1').decode('utf-8') : v for k,v in cn_wordfeats.items()}
 
     file_to_store = open(out_path, "wb")
@@ -82,11 +75,12 @@ def create_nested(clipmodel, pretok, tok_thresh):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--clipmodel', default="clip_github", choices=('clip_github','huggingface'))
+    # parser.add_argument('--clipmodel', default="clip_github", choices=('clip_github','huggingface'))
+    parser.add_argument('--clip_model', default="ViT-B/32", choices=('RN50x4', 'ViT-B/32'))
     parser.add_argument('--pretok', action='store_true')
-    parser.add_argument('--tok_thresh', type=int, default=5)
+    parser.add_argument('--tok_thresh', type=int, default=4)
 
     args = parser.parse_args()
 
-    create_nested(args.clipmodel, args.pretok, args.tok_thresh)
+    create_nested(args.clip_model, args.pretok, args.tok_thresh)
 
---
+++ feature generation/gen_nested_CN.py
@@ -0,0 +1,158 @@
+import csv
+import pickle
+from collections import defaultdict
+import spacy
+from nltk.stem import PorterStemmer
+import argparse
+from tqdm import tqdm
+
+nlp = spacy.load("en_core_web_sm")
+ps = PorterStemmer()
+
+
+def get_singular(token):
+    # returns the singular form of a token, if not plural or no singular form of plural: return False
+    token1 = nlp(token)
+    lemma_tags_plural = {"NNS", "NNPS"}
+
+    for token in token1:
+        lemma = False
+        if token.tag_ in lemma_tags_plural:
+            lemma_temp = token.lemma_
+            if str(lemma_temp) != str(token):
+                lemma = lemma_temp
+        return lemma
+
+def get_plurals(openwords_set):
+    """ returns:
+        total_plural = all in top10th that are plural
+        words_sing = the singular of each of the total_plural  words
+        """
+    # store all the words that are plural
+    total_plural = []
+    for token in openwords_set:
+        lemma = get_singular(token)
+        if lemma:
+            total_plural.append(token)
+
+    # create a lookup dict to go from singular to plural            
+    words_sing = []  # list of all the single words whose plurals are in sing_to_plur
+    sing_to_plur = dict()
+    for plur_word in total_plural:
+        word_sing = get_singular(plur_word)
+        if not word_sing:
+            print("ERROR there is no singular word")
+        if word_sing in words_sing:
+            print("singular already exists",word_sing, plur_word, sing_to_plur[word_sing])
+        sing_to_plur[word_sing] = plur_word
+        words_sing.append(word_sing)
+    return total_plural, sing_to_plur, words_sing
+
+def remove_duplicate_rc(lookupdict):
+    lookupdict2 = dict()
+    for k ,v in lookupdict.items():
+        stored_rel_concepts = []
+        stored_items = []
+        for item in v:
+            rc_idx = int(not(item[-1]))
+            rel_concept = item[rc_idx]
+            if rel_concept in stored_rel_concepts:
+                continue
+            stored_rel_concepts.append(rel_concept)
+            stored_items.append(item)
+        lookupdict2[k] = stored_items
+
+
+
+def parse_rel_label(lookupdict):
+    lookupdict2 = lookupdict.copy()
+    for k,v in lookupdict2.items():
+        for relitem in v:
+            relidx = relitem[-1]
+            relationlabel = relitem[relidx]
+            newrellabel = "<|"+ relationlabel+"|>"
+            relitem[relidx] = newrellabel
+    return lookupdict2
+
+
+def main(openweb10th_path, ConcNet_Eng_path, out_path, args):
+    f = open(openweb10th_path, 'r')
+    with f:
+        reader = csv.reader(f)
+        all_openwebwords = [word[0] for word in reader]
+    openwords_set = set(all_openwebwords)
+    print("number of keywords from file:", len(all_openwebwords))
+    stopwords = nlp.Defaults.stop_words  # load all stopwords
+
+
+    total_plural, sing_to_plur, words_sing = get_plurals(openwords_set)
+    concNet_file = open(ConcNet_Eng_path)
+    csvreader_list = list(csv.reader(concNet_file))
+    tot_csv = len(csvreader_list)
+    concNet_nested_filt = defaultdict(list)
+    for i in tqdm(range(tot_csv)):
+        row = csvreader_list[i]
+        conc_one = str(row[2].split("/")[3]).replace("_", " ")
+        conc_two = str(row[3].split("/")[3]).replace("_", " ")
+        rel = str(row[1].split("/")[-1])
+        if conc_one in stopwords or conc_two in stopwords:
+            continue
+        if conc_one in openwords_set and conc_two in openwords_set:
+            continue 
+
+        stem1 = ps.stem(conc_one)
+        stem2 = ps.stem(conc_two)
+        if stem1 == stem2:
+            continue
+
+        plur1 = sing_to_plur[conc_one] if conc_one in words_sing else conc_one
+        plur2 = sing_to_plur[conc_two] if conc_two in words_sing else conc_two
+
+        # if concone is bird, and plural is birds, an : append idx to birds
+        if plur1 in total_plural and plur1 != conc_one and conc_two not in openwords_set:
+            concNet_nested_filt[plur1].append([rel, conc_two, 0])
+        if plur2 in total_plural and plur2 != conc_two and conc_one not in openwords_set:
+            concNet_nested_filt[plur2].append([conc_one, rel, 1])
+
+        #was here
+        if conc_one in openwords_set:
+            concNet_nested_filt[conc_one].append([rel, conc_two, 0])
+        elif conc_two in openwords_set:
+            concNet_nested_filt[conc_two].append([conc_one, rel, 1])
+    
+    print("Finished creating the filtered nested dict")
+    concNet_nested_filt0 = concNet_nested_filt.copy()
+
+    if not args.no_parse_rel_label:
+        # parse the relationshiplabel
+        concNet_nested_filt1 = parse_rel_label(concNet_nested_filt0)
+        print("Finished parsing relationship labels")
+
+    if not args.allow_duplicate:
+        # remove duplicate related concepts
+        concNet_nested_filt = remove_duplicate_rc(concNet_nested_filt1)
+        print("Finished removing duplicates")
+
+
+    file_to_store = open("concNet_filt_TEMP_basic.pkl", "wb")
+    pickle.dump(concNet_nested_filt0, file_to_store)
+    file_to_store.close()
+    file_to_store = open("concNet_filt_TEMP_parseRelLabel.pkl", "wb")
+    pickle.dump(concNet_nested_filt1, file_to_store)
+    file_to_store.close()
+
+    file_to_store = open(out_path, "wb")
+    pickle.dump(concNet_nested_filt, file_to_store)
+    file_to_store.close()
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--openweb10th_path', default="../../data_files/openwebtext_10th.csv", type=str)
+    parser.add_argument('--ConcNet_Eng_path', default="../../data_files/ass_onlyenglish.csv", type=str)
+    parser.add_argument('--out_path', default="../../data_files/CN_feats/concNet_filt_TEMP2.pkl", type=str)
+
+    parser.add_argument('--allow_duplicate', action='store_true')
+    parser.add_argument('--no_parse_rel_label', action='store_true')
+
+    args = parser.parse_args()
+    main(args.openweb10th_path, args.ConcNet_Eng_path, args.out_path, args)
