21fd9129f06106a50f7a0ba60f2b23f706dddef4
watermeleon <leoneshuijs@gmail.com>
2022-06-28 21:29:15 +0200

    now recycling old feats

--- feature_generation/store_concNet_emb_recycle.py
+++ feature_generation/store_concNet_emb_recycle.py
@@ -36,20 +36,32 @@ def store_conceptnet_embeddings(clipmodel_name, args):
     model, _ = clip.load(clipmodel_name, device=device)
 
     pick_dict = {}
-    out_path = "../../data_files/conceptNet_embedding_"+clipmodel+".pkl"
+    out_path = "../../data_files/conceptNet_embedding_"+clipmodel+args.cn_version+".pkl"
     file_to_store = open(out_path, "wb")
+    if args.use_stored:
+        # load the precomputed conceptnet clip word embeddings
+        cn_wordfeats_path = "../../data_files/CN_feats/conceptNet_embedding_"+clipmodel+".pkl"
+        with open(cn_wordfeats_path, 'rb') as f:
+                cn_wordfeats_stored = pickle.load(f)
+        cn_keyset = set(cn_wordfeats.keys())
 
     word_prefix = ""
     # for word_list in all_conceptnetwords:
     for j in tqdm(range(len(all_conceptnetwords))):
         word_list = all_conceptnetwords[j]
         word = word_prefix + word_list
+        word2 = str(word).encode('latin-1').decode('utf-8')
+        if args.use_stored:
+            if word2 in cn_keyset:
+                prefix = cn_wordfeats_stored[word2]
+                pick_dict[word2] = prefix
+
         # word = "golden gate bridge"
         with torch.no_grad():
             word_tok = clip.tokenize(word).to(device)
             prefix  = model.encode_text(word_tok).squeeze().detach().cpu().numpy()
 
-        word2 = str(word).encode('latin-1').decode('utf-8')
+        # word2 = str(word).encode('latin-1').decode('utf-8')
         pick_dict[word2] = prefix
 
     pickle.dump(pick_dict, file_to_store)
@@ -61,6 +73,8 @@ if __name__ == '__main__':
     # parser.add_argument('--tokenizer', default="clip_github", choices=('clip_github','huggingface'))
     parser.add_argument('--clip_model', default="ViT-B/32", choices=('RN50x4', 'ViT-B/32'))
     parser.add_argument('--device', default="cuda", choices=('cuda','cpu'))
+    parser.add_argument('--cn_version', type=str, default="")
+    parser.add_argument('--use_stored', action='store_true')
 
 
 
