232c3d0b823a962122cb27a1db02849f29fa06fa
watermeleon <leoneshuijs@gmail.com>
2022-06-29 14:34:16 +0200

    gt is tuple so fixed

--- test.py
+++ test.py
@@ -40,7 +40,9 @@ def predict_captions(model, dataloader, spec, transform_tok):
             caps_gen = [transform_tok.decode(sent) for sent in out] 
             caps_gen = [sent.split("<|endoftext|>")[0] for sent in caps_gen]
             print("capsgen:", caps_gen)
-            caps_gt = [sent.encode('latin-1').decode('utf-8') for sent in caps_gt]
+                        print("caps GT:", caps_gt, "\n")
+
+            caps_gt = [tuple([sent.encode('latin-1').decode('utf-8') for sent in img_batch]) for img_batch in caps_gt]
             print("caps GT:", caps_gt, "\n")
             for i, (gts_i, gen_i) in enumerate(zip(caps_gt, caps_gen)):
                 gen['%d_%d' % (it, i)] = [gen_i.strip(), ]
